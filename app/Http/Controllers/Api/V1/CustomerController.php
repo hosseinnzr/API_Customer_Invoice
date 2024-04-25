1eb826368b8d00b3efdc7cd8b8a51e43dd4870cc
hossein nazari <nazari.hossein28@gmail.com>
2024-04-25 15:59:38 +0330

    add update method
    add update method controller with PUT and PACTH method

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Filters\V1\CustomersFilter;
 use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
 use App\Http\Requests\V1\StoreCustomerRequest;
 use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
+
 use App\Models\Customer;
-use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
@@ -62,8 +64,24 @@ class CustomerController extends Controller
     
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        // PUT method for update : must send all method
+        // PATCh method for update : just send method that you want to change
+
+        $customer->update($request->all());
+        
+        return $customer;
     }
+    // example : post method : http://127.0.0.1:8000/api/v1/customers/182 . send json :         
+    //{
+    //     "id": 182,
+    //     "name": "Zulauf-Padberg",
+    //     "type": "I",
+    //     "email": "cole.chelsey@gmail.com",
+    //     "address": "417 Bernardo Vista\nWest Shad, VA 94312-2055",
+    //     "city": "West Nat",
+    //     "state": "Missouri",
+    //     "postal_code": "39709"
+    // }
 
 
     public function destroy(Customer $customer)
